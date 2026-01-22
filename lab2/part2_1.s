fe41842a30e7507a11601bf7fc08c3bdf858661c
Karma Namgyal <karmadnamgyal1@gmail.com>
2026-01-22 15:09:00 -0500

    Update part2_1.s

--- lab2/part2_1.s
+++ lab2/part2_1.s
@@ -1,13 +1,14 @@
-# Connect output to LEDs
-.equ LEDs, 0xFF200000
 .global _start
 _start:
+
+# Connect output to LEDs
+.equ LEDs, 0xFF200000
 
 # s3 should contain the grade of the person with the student number, -1 if not found
 # s0 has the student number being searched
 
     li s0, 718293
-	li t0, 0 # Set a register to hold the index
+	li t0, 0 # Set a register to hold the index 
 	la t1, Snumbers # Pointer to array of student numbers
 	la s1, result # Pointer to where grade is stored
 
@@ -21,35 +22,35 @@ myloop:
 	j myloop # Loop again
 
 found:
-    la t3, Grades # Pointer to array of student grades
-    add  t3, t3, t0 # Pointer to student 718293's grade
-    lb s3, 0(t3) # Load the grade byte into s3
-    sb  s3, 0(s1) # Store the grade into result
-
+	la t3, Grades # Pointer to array of student grades
+    slli t4, t0, 2 # Multiply index by 4 (left-shift of 2-bits)
+    add  t3, t3, t4 # Pointer to student 718293's grade
+    lw  s3, 0(t3) # Store the grade into s3
+    sw  s3, 0(s1) # Store the grade into result
+    
     # Display result on LEDs
     li  t5, LEDs
-    lb  t6, 0(s1)      
-    sw  t6, 0(t5)
-   
+    lw  t6, 0(s1)      
+    sw  t6, 0(t5) 
+    
     j iloop
-   
-failed:
-    li  s3, -1  # Return -1
-    sb  s3, 0(s1) # Store -1 into result
 
+failed:
+    li  s3, -1  # Return -1 
+    sw  s3, 0(s1) # Store -1 into result
+	
     # Display result on LEDs
     li  t5, LEDs
-    lb  t6, 0(s1)      
+    lw  t6, 0(s1)      
     sw  t6, 0(t5)  
-   
+    
 iloop: j iloop
 
 /* result should hold the grade of the student number put into s0, or
--1 if the student number isn't found */
+-1 if the student number isn't found */ 
 
-result: .byte 0 # Result is a byte
-.byte 0,0,0
-.align 2
+result: .word 0
+		
 /* Snumbers is the "array," terminated by a zero of the student numbers  */
 Snumbers: .word 10392584, 423195, 644370, 496059, 296800
         .word 265133, 68943, 718293, 315950, 785519
@@ -57,6 +58,6 @@ Snumbers: .word 10392584, 423195, 644370, 496059, 296800
         .word 467872, 887795, 681936, 0
 
 /* Grades is the corresponding "array" with the grades, in the same order*/
-Grades: .byte 99, 68, 90, 85, 91, 67, 80
-        .byte 66, 95, 91, 91, 99, 76, 68  
-        .byte 69, 93, 90, 72
+Grades: .word 99, 68, 90, 85, 91, 67, 80
+        .word 66, 95, 91, 91, 99, 76, 68  
+        .word 69, 93, 90, 72
