083bfbe9ef5f0fdf84709c5e1059cdaa16134497
karnamgyal <namgyal2006@gmail.com>
2026-02-05 02:04:29 -0500

    Part 2 done

--- lab4/part1.s
+++ lab4/part1.s
@@ -21,22 +21,22 @@ poll:
     sw s0, 0(t2) # Load onto LEDs
     j release
 check_0:
-    andi t3, t1, 0x1        # Check bit 0 
+    andi t3, t1, 0x1 # Check bit 0 
     beq  t3, zero, check_1 # If bit 0 is nonzero, key0 was pressed
     j    key_0
 
 check_1:
-    andi t3, t1, 0x2        # Check bit 1
+    andi t3, t1, 0x2 # Check bit 1
     beq  t3, zero, check_2 # If bit 1 is nonzero, key1 pressed
     j    key_1
 
 check_2:
-    andi t3, t1, 0x4        # Check bit 2
+    andi t3, t1, 0x4 # Check bit 2
     beq  t3, zero, check_3 # If bit 2 is nonzero, key2 pressed
     j    key_2
 
 check_3:
-    andi t3, t1, 0x8        # Check bit 3
+    andi t3, t1, 0x8 # Check bit 3
     beq  t3, zero, release # If bit 3 is nonzero, key3 pressed
     j    key_3
 
@@ -48,15 +48,15 @@ key_0:
 key_1:                     
     li  t4, 15
     beq s0, t4, release # If LEDs already have 15, can't add more (do nothing)
-    addi s0, s0, 1  # Increase otherwise
-    sw  s0, 0(t2)   # Store new value on LEDs
+    addi s0, s0, 1 # Increase otherwise
+    sw  s0, 0(t2) # Store new value on LEDs
     j   release
 
 key_2:                     
     li  t4, 1   
     beq s0, t4, release # If LEDs already have 1, can't subtract more (do nothing)
     addi s0, s0, -1 # Decrease otherwise
-    sw  s0, 0(t2)   # Store new value on LEDs
+    sw  s0, 0(t2) # Store new value on LEDs
     j   release
 
 key_3:                     
@@ -65,6 +65,6 @@ key_3:
     j   release
 
 release:                  
-    lw  t1, 0(t0)   # Read key register
+    lw  t1, 0(t0) # Read key register
     bne t1, zero, release # If its not zero, keep waiting for release
     j   poll
---
+++ lab4/part2.s
@@ -0,0 +1,40 @@
+/* Part 2: Binary counter with delay loops + edge-capture toggle (run/stop) */
+
+.global _start
+_start:
+
+# Define the LEDs
+.equ LEDs, 0xFF200000
+
+# Define the KEY buttons
+.equ KEY_BASE, 0xFF200050
+.equ KEY_EDGE, 0xC 
+
+li s0, 0 # Set counter value to 0 by default
+li t2, LEDs
+sw s0, 0(t2) # Display initial counter value on LEDs
+la t0, KEY_BASE     
+li s1, 0 # Register holding state (counting or stopped)
+
+myloop:
+    lw t1, KEY_EDGE(t0) # Read edge-capture 
+    beq t1, zero, check # If no key event, skip toggle
+    xori s1, s1, 1 # Toggle running flag
+    sw t1, KEY_EDGE(t0) # Clear edge-capture bits by writing 1s (rewrite what was set)
+
+check:
+    beq s1, zero, delay # If stopped, do not increment counter
+    addi s0, s0, 1 # Increment counter
+    andi s0, s0, 0xFF # Mask all bit lower 8 bits
+    sw s0, 0(t2) # Update LEDs with new counter value
+
+delay:
+    call DELAY # Wait about 0.25 seconds
+    j myloop # Repeat forever
+
+DELAY:
+    li t3, 5000000 # Upper bound for delay
+dloop:
+    addi t3, t3, -1 # Countdown
+    bne t3, zero, dloop # Loop until counter hits 0
+    ret                  
