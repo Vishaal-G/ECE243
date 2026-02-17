cf38665a8377dc4907795116a0033b13bc01cd13
karnamgyal <namgyal2006@gmail.com>
2026-02-17 17:40:42 -0500

    Parts 1 and 2 for lab 6

---
+++ lab6/part1.c
@@ -0,0 +1,18 @@
+#include "address_map.h"  // provided by the lab
+
+int main(void) {
+  volatile int* LEDR = (int*)LEDR_BASE;  // 0xFF200000 in Assembly
+  volatile int* KEY = (int*)KEY_BASE;    // 0xFF200050 in Assembly
+
+  // Clear edge capture bits for all keys
+  *(KEY + 3) = 0xF;
+
+  while (1) {
+    int edge = *(KEY + 3);  // Read edge capture: offset 12
+    if (edge & 0x1)         // If KEY0 pressed
+      *LEDR = 0x3FF;        // Turn all LEDs ON
+    if (edge & 0x2)         // If KEY1 pressed
+      *LEDR = 0x0;          // Turn all LEDs OFF
+    *(KEY + 3) = edge;      // Clear edge capture bits
+  }
+}
