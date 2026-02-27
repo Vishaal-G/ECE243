5905f51f6f75349d6b680d28a213441c47ba3267
Karma Namgyal <karmadnamgyal1@gmail.com>
2026-02-27 01:24:47 -0500

    Rename echo buffers and variables for clarity

--- lab6/part4.c
+++ lab6/part4.c
@@ -8,30 +8,30 @@ int main(void) {
   int fifospace; // Variable to hold FIFO space information
 
   // Echo buffers for left and right channels (storing echoed output)
-  static int echoL[DELAY] = {0}; 
-  static int echoR[DELAY] = {0};
-  int idx = 0;
+  static int echo_Left[DELAY] = {0}; 
+  static int echo_Right[DELAY] = {0};
+  int index = 0;
 
   while (1) {
     fifospace = *(audio_ptr + 1); // Read the audio port fifospace register
 
     if ((fifospace & 0x000000FF) > 0) {  // Read from fifo if there is at least one sample
-      int inL = *(audio_ptr + 2); // Read left input sample
-      int inR = *(audio_ptr + 3); // Read right input sample
+      int input_Left = *(audio_ptr + 2); // Read left input sample
+      int input_Right = *(audio_ptr + 3); // Read right input sample
 
       // Echo formula
-      int outL = inL + (echoL[idx] / DAMP_DIV); 
-      int outR = inR + (echoR[idx] / DAMP_DIV);
+      int outL = input_Left + (echo_Left[index] / DAMP_DIV); 
+      int outR = input_Right + (echo_Right[index] / DAMP_DIV);
 
       *(audio_ptr + 2) = outL; // Write left output sample
       *(audio_ptr + 3) = outR; // Write right output sample
 
       // Store current output in echo buffer
-      echoL[idx] = outL; 
-      echoR[idx] = outR; 
+      echo_Left[index] = outL; 
+      echo_Right[index] = outR; 
 
-      idx++;
-      if (idx >= DELAY) idx = 0; // Wrap back to 0 for Circular buffer storage
+      index++;
+      if (index >= DELAY) index = 0; // Wrap back to 0 for Circular buffer storage
     }
   }
 }
